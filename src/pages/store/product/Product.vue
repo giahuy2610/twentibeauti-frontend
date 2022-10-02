350264dc015dae6b7d5320ce9d9553da5b219bdd
Trung Bùi <20520332@gm.uit.edu.vn>
2022-10-02 23:26:35 +0700

    update product

--- src/pages/store/product/Product.vue
+++ src/pages/store/product/Product.vue
@@ -1,6 +1,6 @@
 <template lang="">
     <div class="over">
-        <div class="scroll-wrapper" ref="wrapper">
+        <div class="scroll-wrapper" ref="wrapper" >
             <Button
                 @click="scrollUp"
                 icon="pi pi-angle-up "
@@ -17,7 +17,29 @@
         <div class="img">
           <img src="https://image.hsv-tech.io/400x0/tfs/common/db6c3fd2-2d35-41c3-83ae-af6289768918.webp" alt="Ảnh sản phẩm">
         </div>
-        
+        <div class="infor">
+          <div class="link-item">
+            <nav class="item-breadcrumb">
+              <ol>
+                <li>
+                  <span class="twtenti-breadcrumb-link">
+                    <a role="presentation" class="breadcrumb-link" href="/">Trang chủ</a>
+                  </span>
+                  <span class="twenti-breadcrum-seperator">></span>
+                </li>
+                <li>
+                  <span class="twtenti-breadcrumb-link">
+                    <a role="presentation" class="breadcrumb-link" href="/product"> Sản phẩm</a>
+                  </span>
+                  <span class="twenti-breadcrum-seperator">></span>
+                </li>
+              </ol>
+            </nav>
+
+          </div>
+          
+
+        </div>
     </div>
 </template>
 <script>
@@ -26,26 +48,72 @@
 
 
 <style lang="scss" scoped>
-  .over {
+
+  .over 
+  {
+    
     display: flex;
+    flex-direction: row;
     position: relative;
     width: 100vw;
   }
-  .img
+
+  .item-breadcrumb
   {
-    
+    font-size: 12px;
+    line-height: 18px;
+    text-size-adjust: 100%;
+    ol
+    {
+      list-style-type: none;
+      display: flex;
+      flex-wrap: wrap;
+      margin: 0;
+      padding: 0;
+        li{
+          a{
+            color: #797979 ;
+            background-color: transparent;
+            transition: color .3s;
+            text-decoration: none;
+            touch-action: manipulation;
+          }
+          a:hover
+            {
+              color: #000000;
+              text-decoration: underline;
+            }
+
+          li:last-child > .twenti-breadcrumb-separator 
+          {
+            display: none;
+          }
+          
+          .twenti-breadcrum-seperator
+          {
+            margin-left: 4px;
+            margin-right: 4px;
+          }
+          
+        }
+    }
   }
+
   .scroll-wrapper {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     overflow-x: scroll;
     scroll-behavior: smooth;
-  
+    flex-basis: 250px;
+    
+
+
     &::-webkit-scrollbar {
       display: none;
     }
-    
+
+
     Button{
       left: 10%;
     }
@@ -78,6 +146,8 @@
       max-width: 100%;
       display: block;
       animation: fadeIn 0.8s;
+      
+      
       -webkit-animation: fadeIn 0.8s;
       -moz-animation: fadeIn 0.8s;
       -o-animation: fadeIn 0.8s;
@@ -129,6 +199,8 @@
         }
       }
     }
+
+
   }
 
 
