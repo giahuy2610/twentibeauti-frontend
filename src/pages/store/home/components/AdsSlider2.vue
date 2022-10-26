cbe8eeba0f6375d45f35c09e465939ca47114e6a
AnhTrung732 <20520332@gm.uit.edu.vn>
2022-10-26 18:14:40 +0700

    Update Responsive trungbui

--- src/pages/store/home/components/AdsSlider2.vue
+++ src/pages/store/home/components/AdsSlider2.vue
@@ -1,38 +1,85 @@
 <template lang="">
-<div class="wrapper">
+  <div class="wrapper">
     <div class="wrapper__desktop">
-        <!-- <Skeleton shape="square" size="100px" class="mr-2" v-show="!isLoaded">
+      <!-- <Skeleton shape="square" size="100px" class="mr-2" v-show="!isLoaded">
       </Skeleton> -->
-        <img @click="$router.push({ path: '/collections/'})" v-for="(item, index) in arrThumbnailScr" :src="item" alt="sale thumbnail" @load="isLoaded = index" v-show="isLoaded" @mouseenter="hover = index" @mouseleave="hover = false" :class="{ 'shadow-6': hover === index }" />
+      <img
+        @click="$router.push({ path: '/collections/' })"
+        v-for="(item, index) in arrThumbnailScr"
+        :src="item"
+        alt="sale thumbnail"
+        @load="isLoaded = index"
+        v-show="isLoaded"
+        @mouseenter="hover = index"
+        @mouseleave="hover = false"
+        :class="{ 'shadow-6': hover === index }"
+      />
     </div>
-</div>
-<div class="wrapper">
+  </div>
+  <div class="wrapper">
     <div class="wrapper__mobile">
-        <div class="scroll-wrapper" ref="wrapper">
-            <div class="container-slide" ref="slide">
-                <div class="region">
-                    <div class="gap">
-                        <div class="horizon-scroll">
-                            <button @click="scrollRight(), (isClick = !isClick), (selectedItem = selectedItem + 1)" class="pag__btn btn__up"><span class="icon-chevron-thin-up"></span></button>
-                            <div class="slide-list">
-                                <img v-for="(item, index) in arrThumbnailScr" @click="$router.push({ path: '/collections/'}), (isClick = !isClick), (selectedItem = index)" :src="item" alt="sale thumbnail" :clas="{shadow2: selectedItem == index}" />
-                            </div>
-                            <button @click="scrollLeft(), (isClick = !isClick), (selectedItem = selectedItem - 1)" class="pag__btn btn__down"><span class="icon-chevron-thin-down"></span></button>
-                        </div>
-                    </div>
-                </div>
+      <button v-show="btnLeft" @click="scrollLeft()" class="pag__btn btn__up">
+        <span class="icon-chevron-thin-up"></span>
+      </button>
+      <div class="scroll-wrapper" ref="wrapper">
+        <div class="container-slide" ref="slide">
+          <div class="slick-slider">
+            <div class="slick-list">
+              <div class="slide-track">
+                <img
+                  ref="ads"
+                  v-for="(item, index) in arrThumbnailScr"
+                  @click="
+                    $router.push({ path: '/collections/' }),
+                      (isClick = !isClick)
+                  "
+                  :src="item"
+                  alt="sale thumbnail"
+                  :clas="{ shadow2: selectedItem == index }"
+                />
+              </div>
             </div>
+          </div>
         </div>
+      </div>
+      <button v-show="btnRight" @click="scrollRight()" class="pag__btn btn__down">
+        <span class="icon-chevron-thin-down"></span>
+      </button>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
+  watch: {
+    btnLeft: function () {
+      if (this.$refs.slide.scrollLeft != this.$refs.slide.scrollLeft)
+      {
+        this.btnLeft = true;
+        console.log("Hiii");
+      }
+    },
+  },
+  methods: {
+    scrollLeft() {
+      console.log(this.$refs.slide.scrollLeft, this.$refs.ads[0].offsetWidth);
+      this.$refs.slide.scrollLeft -=
+        (window.innerWidth - (this.arrThumbnailScr.length - 1) * 20) /
+        this.arrThumbnailScr.length;
+    },
+    scrollRight() {
+      console.log(this.$refs.slide.scrollLeft, this.$refs.ads[0].offsetWidth);
+      this.$refs.slide.scrollLeft +=(window.innerWidth - (this.arrThumbnailScr.length - 1) * 20) /this.arrThumbnailScr.length;      
+      console.log("Right");
+    },
+  },
   data() {
     return {
+      btnLeft: false,
+      btnRight: true,
       hover: null,
       isLoaded: false,
+      currentThumbnail: 0,
       arrThumbnailScr: [
         "https://image.hsv-tech.io/1920x640/tfs/common/40f3e5d3-f5ec-4f5f-8d12-245298586d04.webp",
         "https://image.hsv-tech.io/1920x640/tfs/common/7c5ef3a6-3067-4be0-98cc-6a71f5b36ccc.webp",
@@ -85,22 +132,49 @@ export default {
       transition: all 0.3s ease 0s;
       cursor: pointer;
     }
-
-    &:nth-child(2n) {
-      margin: 0 20px;
-    }
   }
 
   &__mobile {
+    position: relative;
+
+    .pag {
+      &__btn {
+        position: absolute;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 48px;
+        height: 48px;
+        background: rgb(255, 255, 255);
+        box-shadow: rgb(184 193 202) 0px 0px 10px;
+        border: none;
+        display: flex;
+        -webkit-box-align: center;
+        align-items: center;
+        -webkit-box-pack: center;
+        justify-content: center;
+        border-radius: 100%;
+        cursor: pointer;
+        z-index: 10;
+        user-select: auto !important;
+      }
+    }
+
+    .btn {
+      &__up {
+        left: 15px;
+      }
+
+      &__down {
+        right: 15px;
+      }
+    }
+
     .scroll-wrapper {
       position: relative;
-      padding-top: 5px;
-      top: 0;
+      left: 0;
       margin-top: 10px;
       display: flex;
       flex-direction: row;
-      scroll-behavior: smooth;
-      flex-basis: 100px;
       align-items: center;
 
       &::-webkit-scrollbar {
@@ -108,68 +182,29 @@ export default {
       }
 
       .container-slide {
-        padding-left: 0px !important;
-        padding-right: 0px !important;
-        display: block;
-        flex: 0 0 100%;
-        max-width: 100%;
-        min-height: 1px;
+        overflow: hidden;
         position: relative;
+        touch-action: pan-x;
+        overflow-x: scroll;
+        scroll-behavior: smooth;
 
-        .region {
+        .slick-slider {
           height: 100%;
 
-          .gap {
-            gap: 10px;
+          .slick-list {
+            width: 100vw;
+            margin: -20px -12px;
 
-            .horizon-scroll {
-              margin: -20px -12px;
-              height: 100%;
-              position: relative;
+            .slide-track {
+              gap: 10px;
+              padding: 20px 12px;
+              -webkit-box-pack: justify;
               display: flex;
-              .slide-list {
-                padding: 20px 12px;
-                -webkit-box-pack: justify;
-                justify-content: space-between;
-                gap: 10px;
-                display: flex;
-                height: max-content;
-                overflow-y: hidden;
-                position: relative;
-                width: 100%;
-              }
-
-              .pag {
-                &__btn {
-                  position: absolute;
-                  top: 50%;
-                  transform: translateY(-50%);
-                  width: 48px;
-                  height: 48px;
-                  background: rgb(255, 255, 255);
-                  box-shadow: rgb(184 193 202) 0px 0px 10px;
-                  border: none;
-                  display: flex;
-                  -webkit-box-align: center;
-                  align-items: center;
-                  -webkit-box-pack: center;
-                  justify-content: center;
-                  border-radius: 100%;
-                  cursor: pointer;
-                  z-index: 10;
-                  user-select: auto !important;
-                }
-              }
-
-              .btn {
-                &__up {
-                  left: 15px;
-                }
-
-                &__down {
-                  right: 15px;
-                }
-              }
+              height: max-content;
+              overflow-y: hidden;
+              position: relative;
+              width: 100%;
+              width: 700px;
             }
           }
         }
