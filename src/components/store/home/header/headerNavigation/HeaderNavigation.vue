b42fff991291f86430eb90b24c568d3c868f6c3e
aiai142 <diuai@aiai142>
2022-12-24 13:25:32 +0700

    navigation

--- src/components/store/home/header/headerNavigation/HeaderNavigation.vue
+++ src/components/store/home/header/headerNavigation/HeaderNavigation.vue
@@ -1,173 +1,170 @@
 <template lang="">
-  <div>
-    <MegaMenu :model="items"> </MegaMenu>
-    <!-- <MegaMenu :model="items" orientation="vertical" /> -->
-  </div>
+  <ul class="main-navigation">
+    <li><a href="#">Khuyến mãi</a></li>
+    <li>
+      <a href="#">Thương hiệu</a>
+      <ul>
+        <li @click="$router.push('/collection/1')"><a>THE FACE SHOP</a></li>
+        <li><a href="">CNP</a></li>
+        <li><a href="">BELIF</a></li>
+        <li><a href="">YEHWADAM</a></li>
+        <li><a href="">AHC</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">Sản phẩm mới</a>
+    </li>
+    <li>
+      <a href="#">Trang điểm</a>
+      <ul>
+        <li>
+          <a href="#">Trang điểm mắt</a>
+          <ul>
+            <li><a href="">Phấn mắt</a></li>
+            <li><a href="">Kẻ viền mắt</a></li>
+            <li><a href="">Kẻ chân mày</a></li>
+            <li><a href="">Mascara</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Trang điểm môi</a>
+          <ul>
+            <li><a href="#">Son lì</a></li>
+            <li><a href="#">Son dưỡng môi</a></li>
+            <li><a href="#">Son môi</a></li>
+            <li><a href="">Son bóng</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Trang điểm mặt</a>
+          <ul>
+            <li><a href="#">Kem nền</a></li>
+            <li><a href="#">Tạo khối</a></li>
+            <li><a href="">Phấn phủ</a></li>
+            <li><a href="">Phấn nước cushion</a></li>
+            <li><a href="">Phấn má hồng</a></li>
+            <li><a href="">Che khuyến điểm</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">Dưỡng da</a>
+      <ul>
+        <li>
+          <a href="#">Dưỡng da mặt</a>
+          <ul>
+            <li><a href="">Kem dưỡng</a></li>
+            <li><a href="">Tinh chất dưỡng da</a></li>
+            <li><a href="">Sữa dưỡng</a></li>
+            <li><a href="">Xịt khoáng</a></li>
+            <li><a href="">Nước cân bằng</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Làm sạch</a>
+          <ul>
+            <li><a href="#">Sữa rửa mặt</a></li>
+            <li><a href="#">Tẩy trang</a></li>
+            <li><a href="#">Tẩy tế bào chết</a></li>
+            <li><a href="">Tẩy tế bào chết môi</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">Mặt nạ</a>
+          <ul>
+            <li><a href="">Mặt nạ dưỡng</a></li>
+          </ul>
+        </li>
+      </ul>
+    </li>
+    <li>
+      <a href="#">Chăm sóc cơ thể</a>
+      <ul>
+        <li><a href="#">Chăm sóc body</a></li>
+        <li>
+          <a href="#">Chăm sóc tóc</a>
+        </li>
+        <li>
+          <a href="#">Chăm sóc tay/chân</a>
+        </li>
+      </ul>
+    </li>
+    <li @click="$router.push('/promotions')"><a>Nhận mã ưu đãi</a></li>
+  </ul>
 </template>
 <script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          label: "Khuyến mãi",
-          url: "/promotion",
 
-          items: [
-            [
-              {
-                items: [
-                  { label: "HOT DEAL ĐỘC QUYỀN THÁNG 10", url: "/promotions" },
-                ],
-              },
-              {
-                items: [
-                  { label: "HOT DEAL ĐỘC QUYỀN THÁNG 10", url: "/promotions" },
-                ],
-              },
-            ],
-          ],
-        },
-        {
-          label: "Thương hiệu",
-          //   icon: "pi-angle-down",
-          items: [
-            [
-              {
-                items: [{ label: "THEFACESHOP", url: "/collections" }],
-              },
-              {
-                items: [{ label: "CNP", url: "/collections" }],
-              },
-            ],
-          ],
-        },
-        {
-          label: "Sản phẩm mới",
-          url: "/collections",
-        },
-        {
-          label: "Trang điểm",
-          url: "/collections",
-
-          items: [
-            [
-              {
-                label: "Trang điểm mặt",
-                items: [
-                  { label: "Kem nền", url: "/collections" },
-                  { label: "Kem lót", url: "/collections" },
-                ],
-              },
-              {
-                label: "Trang điểm môi",
-                items: [
-                  { label: "Dưỡng môi", url: "/collections" },
-                  { label: "Son bóng", url: "/collections" },
-                ],
-              },
-            ],
-            [
-              {
-                label: "Trang điểm mắt",
-                items: [
-                  { label: "Kẻ mắt", url: "/collections" },
-                  { label: "Kẻ lông mày", url: "/collections" },
-                ],
-              },
-            ],
-          ],
-        },
-        {
-          label: "Dưỡng da",
-          url: "collections",
-
-          items: [
-            [
-              {
-                label: "Dưỡng da mặt",
-                items: [
-                  { label: "Dưỡng ẩm", url: "/collections" },
-                  { label: "Tẩy tế bào chết", url: "/collections" },
-                ],
-              },
-              {
-                label: "Làm sạch",
-                items: [
-                  { label: "Sữa rửa mặt", url: "/collections" },
-                  { label: "Nước tẩy trang", url: "/collections" },
-                ],
-              },
-            ],
-            [
-              {
-                label: "Mặt nạ",
-                items: [
-                  { label: "Mặt nạ ngủ", url: "/collections" },
-                  { label: "Mặt nạ dưỡng", url: "/collections" },
-                ],
-              },
-            ],
-          ],
-        },
-        {
-          label: "Chăm sóc cơ thể",
-          url: "/collections",
-
-          items: [
-            [
-              {
-                label: "Chăm sóc BODY",
-                items: [
-                  { label: "Dưỡng cơ thể", url: "/collections" },
-                  { label: "Tẩy tế bào chết ", url: "/collections" },
-                ],
-              },
-              {
-                label: "Chăm sóc tóc",
-                items: [{ label: "Dầu dội" }, { label: "Dầu xả" }],
-              },
-            ],
-            [
-              {
-                label: "Chăm sóc tay/chân",
-                items: [
-                  { label: "Dưỡng da", url: "/collections" },
-                  { label: "Tẩy tế bào chết", url: "/collections" },
-                ],
-              },
-            ],
-          ],
-        },
-        {
-          label: "Nhận mã ưu đãi",
-          url: "/promotions",
-        },
-      ],
-    };
-  },
-};
 </script>
 <style lang="scss" scoped>
-.p-megamenu {
+@import "@/scss/mixin";
+// @media screen and (max-width: 1067px) {
+//   :deep(.p-megamenu .p-megamenu-root-list > .p-menuitem > .p-menuitem-link) {
+//     padding: 0;
+//   }
+// }
+ul {
+  list-style: none;
   padding: 0;
-  border: none;
-  border-radius: 20px;
+  margin: 0;
 }
-:deep(.p-megamenu-root-list) {
-  justify-content: space-between;
-  background-color: white;
-  border-radius: 10px;
-  width: 100%;
-  max-width: 100%;
-  flex-wrap: nowrap !important;
+ul li {
+  display: block;
+  position: relative;
+  float: left;
 }
-:deep(.p-megamenu-panel) {
-  border-radius: 14px;
+li ul {
+  display: none;
 }
-@media screen and (max-width: 1067px) {
-  :deep(.p-megamenu .p-megamenu-root-list > .p-menuitem > .p-menuitem-link) {
-    padding: 0;
+ul li a {
+  display: block;
+  padding: 11px 30px;
+  text-decoration: none;
+  white-space: nowrap;
+  color: #333;
+  font-size: 18px;
+  cursor: pointer;
+  @include mini-tablet {
+    padding: 11px 10px;
+    font-size: 16px;
   }
+  @include tablet {
+    padding: 11px 10px;
+    font-size: 18px;
+  }
+
+
+}
+
+ul li a:hover {
+}
+li:hover > ul {
+  display: block;
+  position: absolute;
+}
+li:hover li {
+  float: none;
+}
+li:hover a {
+  background: #fff;
+
+}
+li:hover li a:hover {
+  background: #edeeef;
+}
+.main-navigation li ul li {
+  border-top: 0;
+}
+ul ul ul {
+  left: 100%;
+  top: 0;
+}
+ul:before,
+ul:after {
+  content: " "; /* 1 */
+  display: table; /* 2 */
+}
+ul:after {
+  clear: both;
 }
 </style>
